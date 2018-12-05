0d89b6ddadfd1c5212f039666311fcbbf1c290ac
Brian Reichle <bwreichle+git@gmail.com>
2018-12-05 22:57:12 +1100

    Convert MethodCheck.Test to new project format.

--- src/MethodCheck.Test/Properties/AssemblyInfo.cs
+++ src/MethodCheck.Test/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("MethodCheck")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 
 [assembly: Guid("97c5075d-94ed-4185-bb87-adeb0ae63029")]
