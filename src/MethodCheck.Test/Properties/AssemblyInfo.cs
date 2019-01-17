3dbeb843245e4854e2fc0f71a98c68b3cf3c011c
Brian Reichle <bwreichle+git@gmail.com>
2019-01-18 08:25:39 +1100

    Removed unnecessary assembly attributes.

--- src/MethodCheck.Test/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("")]
-
-[assembly: Guid("97c5075d-94ed-4185-bb87-adeb0ae63029")]
--- src/MethodCheck/Properties/AssemblyInfo.cs
+++ src/MethodCheck/Properties/AssemblyInfo.cs
@@ -1,12 +1,8 @@
-using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Windows;
 
-[assembly: AssemblyDescription("")]
-
 [assembly: NeutralResourcesLanguage("en-AU")]
-
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 [assembly: InternalsVisibleTo("MethodCheck.Test")]
