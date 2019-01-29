fd1f0a9c6b07a888aeb11c19c6d8803e4cc34f9d
Brian Reichle <bwreichle+git@gmail.com>
2019-01-30 08:07:58 +1100

    Removed unnecessary InternalsVisibleToAttribute.

--- src/MethodCheck/Properties/AssemblyInfo.cs
+++ src/MethodCheck/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Windows;
 
 [assembly: NeutralResourcesLanguage("en-AU")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-[assembly: InternalsVisibleTo("MethodCheck.Test")]
