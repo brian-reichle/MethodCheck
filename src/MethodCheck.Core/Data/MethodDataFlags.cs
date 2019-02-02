77b0f00681e4449bb88a118ce881e6502483de18
Brian Reichle <bwreichle+git@gmail.com>
2019-02-03 09:33:22 +1100

    Added missing copyright notices.

--- src/MethodCheck.Core/Data/MethodDataFlags.cs
+++ src/MethodCheck.Core/Data/MethodDataFlags.cs
@@ -1,3 +1,4 @@
+// Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the MIT License.  See License.txt in the project root for license information.
 using System;
 
 namespace MethodCheck.Core.Data
