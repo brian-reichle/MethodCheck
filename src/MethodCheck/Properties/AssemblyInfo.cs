ad5d5a360d48f5c6b61040cd3f2986d02801aed7
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2020-12-23 19:07:33 +1100

    Update to net5.0.

--- src/MethodCheck/Properties/AssemblyInfo.cs
+++ src/MethodCheck/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Resources;
 using System.Windows;
 
+[assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-AU")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
